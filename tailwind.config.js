6da0c3634fb00f13dac0c9b51372c4a6ee38f44a
Willy Ernesto Russa <93367098+willyernst@users.noreply.github.com>
2023-09-29 13:18:45 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          50 : '#FFA500',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
